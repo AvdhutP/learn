f43921a438ef6d161387c23d3873e497f68e1e91
Avdhut Pandit <avdhutp39@gmail.com>
2020-09-14 01:45:35 +0530

    SingletonDesignPattern

---
+++ src/com/designpatterns/SingletonDesign.java
@@ -0,0 +1,20 @@
+package com.designpatterns;
+
+public class SingletonDesign {
+    /* there are many ways to create singleton pattern
+    best way is creating private static inner class to return the instance
+    */
+
+    private SingletonDesign(){}
+
+    private static class  SignletonDesignHelper
+    {
+        private static final SingletonDesign INSTANCE= new SingletonDesign();
+    }
+
+    public static SingletonDesign getInstance()
+    {
+        return SignletonDesignHelper.INSTANCE;
+    }
+
+}
